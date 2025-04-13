f0fad1a51c93bd321b8c36d1295823ef21dd4d39
Emmanuelsam124 <58230732+Emmanuelsam124@users.noreply.github.com>
2025-04-13 01:35:01 +0100

    fixed frustrating issues

--- script/script.js
+++ script/script.js
@@ -1,4 +1,44 @@
-import { articles } from "../script/articles.js";
+const articles = [
+  {
+    id: "first-article",
+    title: "My journey as a web developer",
+    date: "August 13, 2021",
+    image: "images/emmanuel-sam-blaqcode.webp",
+    author: "Emmanuel A Samuel",
+    description:
+      "I was introduced to programming the same year I returned from my full time mission, by Ojo Rufus Olajide, who was just starting off himself. Programming quickly became my favorite thing from then on.",
+    link: "blog/my-web-developer-journey.html",
+    footer: "Notes",
+    counterId: "result1",
+    onClick: "clickCounter1()",
+  },
+  {
+    id: "second-article",
+    title: "Javascript: My favorite programming language",
+    date: "June 03, 2021",
+    image: "images/emmanuel-sam-blaqcode.webp",
+    author: "Emmanuel A Samuel",
+    description:
+      "To be honest, I only started off my programming career with Python. I fell in love with it, but then, JavaScript took me away from my first love. True, JavaScript has its own perks and worries.",
+    link: "blog/javascript-my-favorite-language.html",
+    footer: "Learn Js",
+    counterId: "result2",
+    onClick: "clickCounter2()",
+  },
+  {
+    id: "third-article",
+    title: "An introduction to web development",
+    date: "January 21, 2021",
+    image: "images/emmanuel-sam-blaqcode.webp",
+    author: "Emmanuel A Samuel",
+    description:
+      "The term web development is all over the internet. It is basically all about the development of websites and web apps, using markup language like HTML, a cascading stylesheet called CSS, and a programming language called JavaScript.",
+    link: "blog/introduction-to-web-development.html",
+    footer: "Javascript",
+    counterId: "result3",
+    onClick: "clickCounter3()",
+  },
+];
 
 document.addEventListener("DOMContentLoaded", function () {
   const container = document.getElementById("articles-container");
